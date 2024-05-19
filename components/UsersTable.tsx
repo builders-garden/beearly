c094903db76a5418faf7f1bbc5e5c87299a31486
lim0n3 <simonestaffa96@gmail.com>
2024-05-19 21:15:39 +0200

    warpcast direct cast integration

--- components/UsersTable.tsx
+++ components/UsersTable.tsx
@@ -7,8 +7,15 @@ import {
   Table,
   Button,
   Image,
+  Tooltip,
 } from "@nextui-org/react";
 import { WaitlistedUser } from "@prisma/client";
+import {
+  ExternalLink,
+  MessageCircleIcon,
+  MessageSquareShare,
+} from "lucide-react";
+import Link from "next/link";
 
 export const UsersTable = ({ users }: { users: WaitlistedUser[] }) => {
   return (
@@ -17,6 +24,12 @@ export const UsersTable = ({ users }: { users: WaitlistedUser[] }) => {
         <TableColumn>NAME</TableColumn>
         <TableColumn>FID</TableColumn>
         <TableColumn>DATE</TableColumn>
+        <TableColumn>
+          <div></div>
+        </TableColumn>
+        <TableColumn>
+          <div></div>
+        </TableColumn>
       </TableHeader>
       <TableBody>
         {users.map((user) => (
@@ -47,6 +60,41 @@ export const UsersTable = ({ users }: { users: WaitlistedUser[] }) => {
             </TableCell>
             <TableCell>{user.fid}</TableCell>
             <TableCell>{new Date(user.waitlistedAt).toDateString()}</TableCell>
+            <TableCell>
+              <Tooltip
+                key={"tooltip-" + user.fid}
+                className="cursor-none"
+                radius="sm"
+                content={
+                  <div className="flex flex-row items-center gap-2">
+                    <Image
+                      className="h-4 w-4"
+                      src="/warpcast-logo.svg"
+                      alt="warpcast-logo"
+                      radius="none"
+                    />
+                    <div>Soon Warpcast Direct Cast integration!</div>
+                  </div>
+                }
+              >
+                <div className="opacity-40">
+                  <Image
+                    className="h-4 w-4"
+                    src="/warpcast-logo.svg"
+                    alt="warpcast-logo"
+                    radius="none"
+                  />
+                </div>
+              </Tooltip>
+            </TableCell>
+            <TableCell>
+              <Link
+                href={`https://warpcast.com/${user.username}`}
+                target="_blank"
+              >
+                <ExternalLink size={16} />
+              </Link>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
