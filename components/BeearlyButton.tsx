607e5e3989f7a2926285ed4483a2fa2e6710b900
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-06-08 16:34:19 +0200

    Added optional Tooltip to the button component

--- components/BeearlyButton.tsx
+++ components/BeearlyButton.tsx
@@ -1,10 +1,13 @@
-import { Button, Link } from "@nextui-org/react";
+import { Button, Link, Tooltip } from "@nextui-org/react";
 import { ReactNode } from "react";
 
 export const BeearlyButton = ({
   onPress,
   icon,
   iconPosition,
+  showTooltip,
+  tooltipText,
+  tooltipPlacement,
   text,
   isLoading,
   isDisabled,
@@ -13,32 +16,41 @@ export const BeearlyButton = ({
   onPress?: () => void;
   icon?: ReactNode;
   iconPosition?: "left" | "right";
+  showTooltip?: boolean;
+  tooltipText?: string;
+  tooltipPlacement?: "top" | "bottom" | "left" | "right";
   text: string;
   isLoading?: boolean;
   isDisabled?: boolean;
   link?: string;
 }) => {
   if (!iconPosition) iconPosition = "left";
+  if (!tooltipPlacement) tooltipPlacement = "top";
   return (
-    <Button
-      color="primary"
-      className="w-fit text-black p-4"
-      onPress={onPress}
-      radius="sm"
-      isLoading={isLoading}
-      isDisabled={isDisabled}
-    >
-      <div className="flex flex-row gap-1 items-center">
-        {iconPosition === "left" && icon}
-        {link ? (
-          <Link href={link} target="_blank">
-            <div className="font-semibold text-black">{text}</div>
-          </Link>
-        ) : (
-          <div className="font-semibold">{text}</div>
-        )}
-        {iconPosition === "right" && icon}
+    <Tooltip isDisabled={!showTooltip} showArrow={true} placement={tooltipPlacement} content={tooltipText}>
+      {/* This div is needed to make the Tooltip work on disabled buttons */}
+      <div>
+        <Button
+          color="primary"
+          className="w-fit text-black p-4"
+          onPress={onPress}
+          radius="sm"
+          isLoading={isLoading}
+          isDisabled={isDisabled}
+        >
+          <div className="flex flex-row gap-1 items-center">
+            {iconPosition === "left" && icon}
+            {link ? (
+              <Link href={link} target="_blank">
+                <div className="font-semibold text-black">{text}</div>
+              </Link>
+            ) : (
+              <div className="font-semibold">{text}</div>
+            )}
+            {iconPosition === "right" && icon}
+          </div>
+        </Button>
       </div>
-    </Button>
+    </Tooltip>
   );
 };
