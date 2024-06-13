e5fc16e56ef2bd3e7c34e423563f8d6338b6c94c
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-06-13 19:13:19 +0200

    Moved cron job in public api route (the CRON_SECRET will protect it)

--- app/api/public/cron/sync-users/route.ts
+++ app/api/public/cron/sync-users/route.ts
@@ -1,34 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Client } from "@upstash/qstash";
-
-export async function loadQstash(
-  endpoint: string,
-  offset: number
-): Promise<{ response: string }> {
-  // Get the Qstash client
-  const qstashClient = new Client({
-    token: process.env.QSTASH_TOKEN!, // Add this to .env file
-  });
-
-  // Send payload to the Qstash API
-  try {
-    await qstashClient.publishJSON({
-      url: endpoint,
-      body: {
-        offset: offset,
-      },
-    });
-    return { response: "ok" };
-  } catch (error) {
-    console.error("Error while publishing json to QStash: ", error);
-    return { response: "ko" };
-  }
-}
+import { loadQstash } from "../../../../../lib/qstash";
 
 export const GET = async (req: NextRequest) => {
   const authHeader = req.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", {
+    return new NextResponse("Unauthorized", {
       status: 401,
     });
   }
