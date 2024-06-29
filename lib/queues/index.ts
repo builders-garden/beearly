65da8de17dba4115b4d8ffdb080fab8abcfe1b0f
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-06-29 16:39:07 +0200

    Created function to add an XMTP message in the Redis queue

--- lib/queues/index.ts
+++ lib/queues/index.ts
@@ -3,9 +3,20 @@ import {
   dcsQueue,
   processDC,
 } from "./direct-casts";
+import {
+  MessageWithAddressBody,
+  processXMTPmessage,
+  xmtpQueue,
+} from "./xmtp-messages";
 
+// Constants to define the job names
 const DCS_JOB_NAME = "create-dc";
+const XMTP_JOB_NAME = "send-xmtp";
 
+/**
+ * @dev this function adds a new job to the dcs queue, or processes it directly if the queue is not available.
+ * @param {MessageWithFarcasterIdBody} data the data to put in the queue or process directly
+ */
 export const addToDCsQueue = async (data: MessageWithFarcasterIdBody) => {
   if (dcsQueue) {
     console.log(`[dcs] [${Date.now()}] - adding dc to queue.`);
@@ -14,4 +25,18 @@ export const addToDCsQueue = async (data: MessageWithFarcasterIdBody) => {
   }
   console.log(`[dcs] [${Date.now()}] - processing dc directly.`);
   await processDC({ data });
+};
+
+/**
+ * @dev this function adds a new job to the xmtp queue, or processes it directly if the queue is not available.
+ * @param {MessageWithAddressBody} data the data to put in the queue or process directly
+ */
+export const addToXMTPQueue = async (data: MessageWithAddressBody) => {
+  if (xmtpQueue) {
+    console.log(`[xmtp] [${Date.now()}] - adding xmtp message to queue.`);
+    await xmtpQueue.add(XMTP_JOB_NAME, data);
+    return;
+  }
+  console.log(`[xmtp] [${Date.now()}] - processing xmtp message directly.`);
+  await processXMTPmessage({ data });
 };
