ea13382778bb7ff02fc1451a8f8c054b175a2cea
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-07-02 16:55:18 +0200

    Added returns based on error handling

--- lib/farcaster.ts
+++ lib/farcaster.ts
@@ -7,9 +7,8 @@ import { v4 as uuidv4 } from "uuid";
 export const sendDirectCast = async (recipient: number, text: string) => {
   if (!process.env.WARPCAST_API_KEY) {
     console.log("No WARPCAST_API_KEY found, skipping direct cast send.");
-    return;
+    return { ok: false };
   }
-
   const res = await fetch("https://api.warpcast.com/v2/ext-send-direct-cast", {
     method: "PUT",
     headers: {
@@ -24,7 +23,8 @@ export const sendDirectCast = async (recipient: number, text: string) => {
   });
   if (!res.ok) {
     console.error(`error sending direct cast to ${recipient}.`);
-    return;
+    return { ok: false };
   }
   console.log(`direct cast sent to ${recipient}.`);
+  return { ok: true };
 };
