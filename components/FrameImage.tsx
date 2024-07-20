a56b2ed6859929423ce98c9645db90628a931a73
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-07-20 15:32:08 +0200

    Modified onSelectImageFile function to evaluate file dimension and show an error toast if it's over 1MB

--- components/FrameImage.tsx
+++ components/FrameImage.tsx
@@ -1,10 +1,20 @@
 import { Image } from "@nextui-org/react";
 import { ImageIcon, PlusSquare } from "lucide-react";
+import toast from "react-hot-toast";
 
 export const onSelectedImageFile =
-  (setUploadedImage: (s: string) => void, setSelectedFile: (f: any) => void) =>
+  (setUploadedImage: (s: string) => void, setSelectedFile: (f: File) => void) =>
+  (fileInputId: string) =>
   (event: any) => {
-    if (!event?.target?.files[0]) return;
+    const file = event.target.files[0];
+    // Check if file is there
+    if (!file) return;
+    // Check if file size is greater than 1MB
+    if (file && file.size > 1000000) {
+      toast.error("File size is greater than 1MB", { position: "top-right" });
+      (document.getElementById(fileInputId) as HTMLInputElement).value = "";
+      return;
+    }
     const reader = new FileReader();
     reader.addEventListener("load", () => {
       setUploadedImage(reader.result?.toString() || "");
@@ -21,7 +31,7 @@ export const FrameImage = ({
 }: {
   selectedFile: File;
   uploadedImage: string;
-  onSelectedFile: (e: any) => void;
+  onSelectedFile: (fileInputId: string) => (e: any) => void;
   label: string;
 }) => {
   const fileInputId = `${label}-file-input`;
@@ -40,7 +50,7 @@ export const FrameImage = ({
         </div>
       ) : (
         <div className="text-gray-400 w-[287px] bg-white my-2 h-[150px] rounded-md hover:border-primary hover:border-2 hover:border-dashed hover:text-primary ">
-          <div className="flex flex-col gap-1 justify-center items-center h-full my-auto  hover:text-primary">
+          <div className="flex flex-col gap-1 justify-center items-center h-full my-auto">
             <PlusSquare size={24} />
             <div className="text-lg">Add image</div>
           </div>
@@ -50,7 +60,7 @@ export const FrameImage = ({
         type="file"
         id={fileInputId}
         className="hidden"
-        onChange={onSelectedFile}
+        onChange={onSelectedFile(fileInputId)}
       />
       <div className="flex flex-row gap-2 items-center justify-center pb-2">
         <ImageIcon size={16} className="text-gray-500" />
