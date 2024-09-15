062d60223dcf752152b22c7a68e0d9d26625d48e
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-15 12:47:01 +0200

    Removed default image options and added image worker middleware

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -2,38 +2,16 @@ import { farcasterHubContext, openframes } from "frames.js/middleware";
 import { createFrames } from "frames.js/next";
 import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
 import { FRAMES_BASE_PATH, appURL } from "../utils";
-import { interBoldFontData } from "../../lib/fonts";
-
-interface ImageOptions {
-  aspectRatio: "1.91:1" | "1:1";
-  height: number;
-  width: number;
-  fonts: {
-    data: Buffer | ArrayBuffer;
-    name: string;
-    weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
-    style?: "normal" | "italic";
-    lang?: string;
-  }[];
-}
-
-export const defaultImageOptions: ImageOptions = {
-  aspectRatio: "1.91:1",
-  height: 1000,
-  width: 1910,
-  fonts: [
-    {
-      name: "Inter",
-      data: interBoldFontData,
-      weight: 700,
-    },
-  ],
-};
+import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker";
 
 export const frames = createFrames({
   basePath: FRAMES_BASE_PATH,
   baseUrl: appURL(),
   middleware: [
+    imagesWorkerMiddleware({
+      imagesRoute: "/images",
+      secret: process.env.IMAGES_WORKER_SECRET as string,
+    }),
     farcasterHubContext({
       ...(process.env.NODE_ENV === "production"
         ? {
